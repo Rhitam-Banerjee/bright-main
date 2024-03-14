35e130e2c06919f7494b4aa9a464e05f82732a33
Brightrclub <banerjeerhitam1@gmail.com>
2024-03-14 15:55:21 +0530

    UPDATE:Added FAQs

--- src/components/Landing/constants.js
+++ src/components/Landing/constants.js
@@ -80,28 +80,218 @@ export const bookVideos = [
 
 export const allFaqs = [
   {
-    ques: "There are many variations of passages of Lorem Ipsum available",
-    ans: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,",
+    ques: "How does BrightR.club work ?",
+    ans: (
+      <html>
+        <h1 className="text-[10px] font-bold">Step 1</h1>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Register and Become a Member Sign up and become a member to access all
+          our fantastic books
+        </p>
+        <h1 className="text-[10px] font-bold">Step 2:</h1>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Explore Our Library Choose books by age, series, genres, or pick from
+          special plans.
+        </p>
+        <h1 className="text-[10px] font-bold">Step 3:</h1>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Magical Delivery Our speedy delivery heroes bring books to your
+          doorstep within 24 hours. Step 4: Weekly Book Exchange Get reminders
+          from our WhatsApp team for a weekly book exchange. Build the best
+          reading habit with new adventures each week!
+        </p>
+      </html>
+    ),
     open: true,
   },
   {
-    ques: "There are many variations of passages of Lorem Ipsum available",
-    ans: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,",
+    ques: "What is the process for borrowing books ?",
+    ans: (
+      <html>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Borrowing books from BrightR.club is as easy as pie!
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Dive into our treasure trove of books, sorted by age, series, genres,
+          or special reading plans. You can make your very own wish-list of all
+          your favourite books. It's like picking out the adventures you want to
+          go on, but in book form!
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          You have the magical choice of picking your own books from our library
+          or choosing from our specially curated reading plans. These plans are
+          like treasure maps to fantastic adventures, handpicked just for your
+          age group and interests. It's an easy way to discover new favourites
+          without searching!
+        </p>
+      </html>
+    ),
     open: false,
   },
   {
-    ques: "There are many variations of passages of Lorem Ipsum available",
-    ans: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,",
+    ques: "How do I return the book ?",
+    ans: (
+      <html>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Returning books to BrightR.club is as easy as a wave of your wand! Our
+          friendly delivery team, who comes to deliver your new adventures, will
+          also collect the previous books from you.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          When they bring your fresh batch of books, simply hand over the ones
+          you've finished reading. They will take care of everything and make
+          sure those books find their way back to our enchanted library.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          So, you don't have to worry about a thing! It's all part of the magic
+          of BrightR.club, where reading is a delightful adventure from start to
+          finish. Happy reading and returning! 📚🚚✨
+        </p>
+      </html>
+    ),
     open: false,
   },
   {
-    ques: "There are many variations of passages of Lorem Ipsum available",
-    ans: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,",
+    ques: "Can you help me choose books for my children ?",
+    ans: (
+      <html>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          That's why every book in our enchanted library comes with a treasure
+          trove of information, including Amazon reviews and ratings. You can
+          see what other young adventurers and their grown-ups have to say about
+          each book, making it easier to pick the ones that match your child's
+          interests.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          But wait, there's more enchantment! We've added categories like "New
+          York Times Bestsellers" and other book awarding agencies to help you
+          discover the best books that have been loved by readers all over the
+          world.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          And here's where the magic really sparkles: Our in-house dedicated
+          librarians, armed with their wisdom and a sprinkle of AI, can create
+          customised reading plans just for you! They'll curate a collection of
+          books tailored to your child's age, interests, and reading level,
+          making sure each adventure is a perfect fit.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          So, whether you're exploring on your own with Amazon reviews and
+          ratings, following the trail of bestsellers, or seeking personalised
+          guidance from our librarians, you're in for a magical reading journey
+          with BrightR.club! 📚🌟✨
+        </p>
+      </html>
+    ),
     open: false,
   },
   {
-    ques: "There are many variations of passages of Lorem Ipsum available",
-    ans: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,",
+    ques: "What types of books do you offer ?",
+    ans: (
+      <html>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          BrightR.club is a magical world of books for young explorers aged 0 to
+          12! We have an endless collection that goes beyond just stories. Dive
+          into the pages of our enchanted library and discover a wide array of
+          books. You can explore factual books packed with amazing facts,
+          discover encyclopaedias filled with knowledge on various subjects,
+          embark on thrilling adventures, unlock mysteries, and even learn about
+          the wonders of science.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          And for our youngest growing readers aged 0 to 5 years, we have
+          something truly special. Our collection includes board books that are
+          perfect for little hands, touch and feel books to engage their senses,
+          and international best sellers crafted just for them. BrightR.club is
+          here to nurture a love for reading from the very beginning, making
+          every story-time a magical experience. So, get ready to embark on
+          countless adventures and unlock the doors to knowledge with
+          BrightR.club! 📚🌟✨
+        </p>
+      </html>
+    ),
+    open: false,
+  },
+  {
+    ques: "How often do you deliver books ?",
+    ans: (
+      <html>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          At BrightR.club, we're on a magical mission to help you build a love
+          for reading, and that's why we deliver books to your doorstep every
+          single week! We believe that the more books you have, the more
+          adventures you can embark on.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          But here's the best part: we understand that each young reader is
+          unique, and so are your reading habits. That's why we're happy to
+          accommodate your delivery preferences. Whether you prefer books to be
+          delivered on specific days or spread throughout the week, we're here
+          to make it as enchanting as possible for you!
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          So, get ready for a weekly dose of reading excitement with
+          BrightR.club. We're here to make sure you always have new stories to
+          explore and keep that reading habit shining bright! 📚🌟✨
+        </p>
+      </html>
+    ),
+    open: false,
+  },
+  {
+    ques: "What happens if a book is damaged or lost while returning it ?",
+    ans: (
+      <html>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          We completely understand that accidents can happen, even in the most
+          magical of adventures. At BrightR.club, we are a library that shares
+          the joy of reading, and we're here to make your reading experience
+          enchanting.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          If a book happens to be damaged during its journey to your doorstep or
+          while it's in your care, don't worry at all! We understand that a
+          little wear and tear can occur, and there won't be any charges for
+          damaged books.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          However, if a book goes missing while it's in your care, when
+          returning it, we kindly ask that you replace the lost book or cover
+          the cost. This helps ensure that all young adventurers have access to
+          our magical library and can continue their reading journeys.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Our goal is to make reading delightful and accessible, so you can
+          always count on us to ensure you have the most delightful stories in
+          your hands. Happy reading, young adventurer! 📚✨
+        </p>
+      </html>
+    ),
+    open: false,
+  },
+  {
+    ques: "What are the duration options for membership ?",
+    ans: (
+      <html>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          We offer a standard 6-month membership because we understand that
+          building a deep and lasting connection with books takes time. This
+          duration allows young readers to immerse themselves in countless
+          stories, savouring each adventure and truly falling in love with the
+          magic of reading.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          Short-term memberships may come and go, but with a 6-month membership
+          at BrightR.club, you have the opportunity to develop a strong reading
+          habit, discover new favourites, and embark on a journey that will last
+          a lifetime.
+        </p>
+        <p className="text-[10px] text-unHighlightDark pl-2">
+          So, join us for six months of enchanting reading adventures, and let
+          the love for books grow and flourish in your young reader's heart!
+        </p>
+      </html>
+    ),
     open: false,
   },
 ];
