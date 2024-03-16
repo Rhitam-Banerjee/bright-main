80e1ded77d68192a2a30d94d036912da1b04347a
Brightrclub <banerjeerhitam1@gmail.com>
2024-03-16 11:23:05 +0530

    FIX:FAQs scroll

--- src/components/Header/NewHeader.jsx
+++ src/components/Header/NewHeader.jsx
@@ -82,17 +82,6 @@ const NewHeader = () => {
     }
   }, []);
 
-  useEffect(() => {
-    if (location.hash) {
-      let elem = document.getElementById(location.hash.slice(1));
-      if (elem) {
-        elem.scrollIntoView({ behavior: "smooth" });
-      }
-    } else {
-      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    }
-  }, []);
-
   return (
     <nav
       className={`fixed ${
